1b1eec1442915c69e319751da1d35d423553e758
Nacho Caiafa <ignacio.caiafa@gmail.com>
2026-02-04 21:21:03 -0300

    Add local env

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,21 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.sass" {
+  const content: string;
+  export default content;
+}
+
 import "./.next/dev/types/routes.d.ts";
 
 // NOTE: This file should not be edited
